43ab4462778d490a16b6f85620d81c2a2a798dc8
Yohannes Getu <nede.evoli@gmail.com>
2021-05-31 09:54:14 +0300

    added recommending best match feature

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import json
+from difflib import get_close_matches
 
 data = json.load(open("data.json"))
 
@@ -6,6 +7,10 @@ def translate(word):
     word = word.lower()
     if word in data:
         return data[word]
+
+    matched_data = get_close_matches(word, data.keys()) 
+    if len(matched_data) > 0:
+        return "Did you mean %s instead?" % matched_data[0]
     else:
         return "Rhe word doesn't exist. Please double check it."
 
